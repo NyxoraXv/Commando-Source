c16bf1f3ef43f5f8f0a9e3d06e538661b3f85d1f
Xavena <87947067+XavenaXv@users.noreply.github.com>
2023-10-19 21:29:32 +0700

    5

---
+++ Assets/Scripts/UI/Change Weapon/Card OnClick.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardClick : MonoBehaviour
+{
+    public enum Weapon
+    {
+        Glock,
+        Revolver
+    }
+
+
+
+    public void OnClick() {
+        
+    }
+}
