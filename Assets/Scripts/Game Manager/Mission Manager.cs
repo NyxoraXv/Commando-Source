0370567bd7810d0b89d28fd1465a625d0f1e6153
NyxoraXv <87947067+NyxoraXv@users.noreply.github.com>
2024-07-20 08:39:51 +0700

    adjustmen

---
+++ Assets/Scripts/Game Manager/Mission Manager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Difficulty
+{
+    Easy,
+    Intermediate,
+    Hunter,
+    Professional,
+    Chainkiller
+}
+
+public enum RewardType
+{
+    XP,
+    Gold,
+    Diamond,
+    Item
+}
+
+public class MissionManager : MonoBehaviour
+{
+    public static MissionManager Instance;
+    public int onLoaded = 0;
+
+    [System.Serializable]
+    public class RewardData
+    {
+        public RewardType Type;
+        public int Value;
+    }
+
+    [System.Serializable]
+    public class missionData
+    {
+        [Header("Mission Information")]
+        public string MissionName;
+        public Difficulty Difficulty;
+
+        [Header("Mission Objectives")]
+        public List<string> Objectives;
+
+        [Header("Mission Rewards")]
+        public List<RewardData> Rewards; // List of rewards with type and value.
+
+        [Header("Mission Status")]
+        public bool Completed;
+
+        [Header("Mission Int")]
+        public int Level;
+    }
+
+    public List<missionData> MissionInformation;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+}
